38063f8f7f80a4597bc112f23780fbb885d35d0a
EkamBehl <ekambehl@gmail.com>
2023-09-21 20:55:30 +1000

    modified

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -7,7 +7,7 @@ export const Billboard:React.FC<BillboardProps> = ({
     data
 }) => {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden bg-cover">
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden bg-contain">
         <div 
             className=" rounded-xl relative aspect-square md:aspect-[2.4/1]" 
             style={{backgroundImage: `url(${data.imageUrl})`}}>
